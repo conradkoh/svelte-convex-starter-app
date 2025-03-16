1b5d0a717fdf7b5240a4e024d6854f01a666685b
conradkoh <conradkoh@gmail.com>
2025-03-17 00:33:15 +0800

    integrate svelte with convex

--- apps/webapp/src/routes/+page.svelte
+++ apps/webapp/src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang='ts'>
+  import { useQuery } from 'convex-svelte';
+  import { api } from '@workspace/backend/convex/_generated/api';
+  const appinfo = useQuery(api.appinfo.get, {});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<footer>
+  <div>App version: {appinfo.data?.version}</div>
+</footer>
